aeef1ea7fe84776d89fb76b46a538e1c36187514
Anastasia-11 <55688203+Anastasia-11@users.noreply.github.com>
2021-04-18 23:40:39 +0300

    Add files via upload

---
+++ OS_lab_3/Params1.h
@@ -0,0 +1,21 @@
+//
+// Created by ASUS on 16.04.2021.
+//
+
+#ifndef OS_LAB3_PARAMS1_H
+#define OS_LAB3_PARAMS1_H
+
+#include <vector>
+
+class Params1 {
+public:
+    int row1;
+    int row2;
+
+    Params1(const int& row1, const int& row2)
+            : row1(row1), row2(row2) {}
+
+};
+
+
+#endif //OS_LAB3_PARAMS1_H
---
+++ OS_lab_3/cmake-build-debug/CMakeFiles/OS_lab_3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_lab_3.dir/Params1.cpp.obj"
+  "CMakeFiles/OS_lab_3.dir/Params2.cpp.obj"
+  "CMakeFiles/OS_lab_3.dir/Timer.cpp.obj"
+  "CMakeFiles/OS_lab_3.dir/main.cpp.obj"
+  "OS_lab_3.exe"
+  "OS_lab_3.exe.manifest"
+  "OS_lab_3.lib"
+  "OS_lab_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_lab_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
